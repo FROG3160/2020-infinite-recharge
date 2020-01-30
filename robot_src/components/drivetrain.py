08c7c2112c542b829f53ccdd6aaca21b7f0367f8
Brian Jarrett <celttechie@gmail.com>
2020-01-29 21:51:29 -0600

    moved constants out to the module

--- robot_src/components/drivetrain.py
+++ robot_src/components/drivetrain.py
@@ -3,11 +3,38 @@ from ctre import WPI_TalonFX, ControlMode, NeutralMode, FeedbackDevice, PigeonIM
 from wpilib.drive import DifferentialDrive
 import math
 from networktables import NetworkTables
-from .common import PID
+from .common import PID, remap, limit
 
 # from subsystems.vision import FROGVision
 # from subsystems.common import PID
 
+# drivetrain characteristics.  Values are changed from last year.
+# The encoders are now on the motor shaft instead of the drive shaft
+# and the encoder has half the resolution of the ones from last year.
+ENCODER_TICKS_PER_REV = 2048*10.71  # motor ticks * gear reduction
+MAX_VELOCITY = 18000  # Falcons are maxing at 20k - 21k
+MAX_ACCEL = 9000/50 # change of 9000 / 50
+MAX_DECEL = MAX_ACCEL
+WHEEL_DIAMETER = 6
+TICKS_PER_INCH = ENCODER_TICKS_PER_REV / (math.pi * WHEEL_DIAMETER)
+TICKS_PER_ANGLE = (8000 * 10.71) / 180
+
+# PIDs for drivetrain
+VelocityPID = PID(slot=0, f=0.313)
+PositionPID = PID(slot=0, f=0.320, p=0.55)
+RotatePID = PID(slot=0, f=0.320, p=0.76)
+TurnPID = PID(p=0.035, d=0.10, i=0.001)
+PIDOutputLimit = 0.66
+
+TURNDEADBAND = 1.5  # in degrees
+
+# Motor Control modes
+VELOCITY_MODE = ControlMode.Velocity
+POSITION_MODE = ControlMode.MotionMagic
+
+# Motion Magic settings
+MM_ACCELERATION = 5000
+MM_CRUISE_VELOCITY = 10000
 
 class FROGGyro(PigeonIMU):
 
@@ -25,46 +52,11 @@ class FROGGyro(PigeonIMU):
 
 class FROGDrive(DifferentialDrive):
 
-    # drivetrain characteristics.  Values are changed from last year.
-    # The encoders are now on the motor shaft instead of the drive shaft
-    # and the encoder has half the resolution of the ones from last year.
-    ENCODER_TICKS_PER_REV = 2048*10.71  # motor ticks * gear reduction
-    MAX_VELOCITY = 18000  # Falcons are maxing at 20k - 21k
-    WHEEL_DIAMETER = 6
-    TICKS_PER_INCH = ENCODER_TICKS_PER_REV / (math.pi * WHEEL_DIAMETER)
-    TICKS_PER_ANGLE = (8000 * 10.71) / 180
-
-    # PIDs for drivetrain
-    VelocityPID = PID(slot=0, f=0.313)
-    PositionPID = PID(slot=0, f=0.320, p=0.55)
-    RotatePID = PID(slot=0, f=0.320, p=0.76)
-    TurnPID = PID(p=0.035, d=0.10, i=0.001)
-    PIDOutputLimit = 0.66
-    TURNDEADBAND = 1.5  # in degrees
-
-    # Motor Control modes
-    VELOCITY_MODE = ControlMode.Velocity
-    POSITION_MODE = ControlMode.MotionMagic
-
-    # Motion Magic settings
-    MM_ACCELERATION = 5000
-    MM_CRUISE_VELOCITY = 10000
-
-    # TODO: change to tunables?  I think thesea are only needed
-    # to hold values to place on network tables
-    left_encoder_current = 0
-    right_encoder_current = 0
-
     # drive motors (channels defined in robot.py)
     leftMaster: WPI_TalonFX
     rightMaster: WPI_TalonFX
     leftSlave: WPI_TalonFX
     rightSlave: WPI_TalonFX
-
-    # motor value for each side of the drivetrain
-    left_control = None
-    right_control = None
-    control_mode = VELOCITY_MODE
 
     def __init__(self):
         # init can't be used for setting up magic components because
@@ -80,8 +72,8 @@ class FROGDrive(DifferentialDrive):
 
     def config_motion_magic(self):
         for motor in [self.leftMaster, self.rightMaster]:
-            motor.configMotionAcceleration(self.MM_ACCELERATION, 0)
-            motor.configMotionCruiseVelocity(self.MM_CRUISE_VELOCITY, 0)
+            motor.configMotionAcceleration(MM_ACCELERATION, 0)
+            motor.configMotionCruiseVelocity(MM_CRUISE_VELOCITY, 0)
 
     def init_nt(self):
         NetworkTables.initialize()
@@ -90,14 +82,14 @@ class FROGDrive(DifferentialDrive):
         self.vision_table = NetworkTables.getTable("Vision")
 
     def init_position_drive(self):
-        self.setPID(self.PositionPID)
+        self.setPID(PositionPID)
         self.reset_control_values()
-        self.drivemode = self.POSITION_MODE
+        self.drivemode = POSITION_MODE
 
     def init_velocity_drive(self):
-        self.setPID(self.VelocityPID)
+        self.setPID(VelocityPID)
         self.reset_control_values()
-        self.drivemode = self.VELOCITY_MODE
+        self.drivemode = VELOCITY_MODE
 
     def reset_control_values(self):
         self.left_control = None
@@ -129,7 +121,7 @@ class FROGDrive(DifferentialDrive):
         self.rightMaster.setNeutralMode(NeutralMode.Coast)
 
     def setup(self):
-        #
+        # called by MagicBot after object construction
         self.set_motor_slaves()
         self.set_motor_output()
         self.set_motor_neutral_mode()
@@ -189,21 +181,22 @@ class FROGDrive(DifferentialDrive):
         # leftMotorSpeed = wpilib.RobotDrive.limit(leftMotorSpeed)
         # rightMotorSpeed = wpilib.RobotDrive.limit(rightMotorSpeed)
 
-        self.left_control = leftMotorSpeed * self.MAX_VELOCITY
-        self.right_control = rightMotorSpeed * self.MAX_VELOCITY
-        self.control_mode = self.VELOCITY_MODE
-
+        self.left_control = leftMotorSpeed * MAX_VELOCITY
+        self.right_control = rightMotorSpeed * MAX_VELOCITY
+        
+        self.control_mode = VELOCITY_MODE
+        
     def setPosition(self, distance):
 
         if distance:
             self.left_control = (
-                distance * self.TICKS_PER_INCH
+                distance * TICKS_PER_INCH
             ) + self.leftMaster.getSelectedSensorPosition(0)
             self.right_control = (
-                distance * self.TICKS_PER_INCH
+                distance * TICKS_PER_INCH
             ) + self.rightMaster.getSelectedSensorPosition(0)
-        self.control_mode = self.POSITION_MODE
-        self.setPID(self.PositionPID)
+        self.control_mode = POSITION_MODE
+        self.setPID(PositionPID)
 
     def setRotate(self, angle):
         if angle:
@@ -211,8 +204,8 @@ class FROGDrive(DifferentialDrive):
 
             self.left_control = ticks + self.leftMaster.getSelectedSensorPosition()
             self.right_control = -ticks + self.rightMaster.getSelectedSensorPosition()
-            self.control_mode = ControlMode.MotionMagic
-            self.setPID(self.RotatePID)
+            self.control_mode = POSITION_MODE
+            self.setPID(RotatePID)
 
     def setTargetAngle(self):
         target_angle = self.vision.getTargetAngle()
