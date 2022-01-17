92c4fa54185a1fc64d5d6711c08b007f88386896
Brian Jarrett <celttechie@gmail.com>
2022-01-16 22:26:03 -0600

    FROGStick changes, take 2

--- robot_src/components/driverstation.py
+++ robot_src/components/driverstation.py
@@ -13,7 +13,7 @@ LEFT_RUMBLE = GenericHID.RumbleType.kLeftRumble
 RIGHT_RUMBLE = GenericHID.RumbleType.kRightRumble
 
 
-class FROGStick(Joystick):
+class FROGStickBase(Joystick):
 
     DEADBAND = 0.15
     SPEED_DIVISOR = 1
@@ -22,33 +22,38 @@ class FROGStick(Joystick):
     ROTATION_MAX = 0.5
     DEBOUNCE_PERIOD = 0.5
 
-    def __init__(self, channel):
+    def __init__(self, port, name='FROGStick'):
+        super().__init__(port)
+        self.name = name
+        self.port = port
+        # set channel numbers for the joystix axis
+        # this can be determined in the driver station
+        self.setXChannel(0)
+        self.setYChannel(1)
+        self.setThrottleChannel(2)
+        self.setTwistChannel(3)
 
-        super().__init__(channel)
-        self.setThrottleChannel(3)
-        self.setTwistChannel(2)
         self.button_latest = {}
         self.timer = wpilib.Timer
-        self.nt = NetworkTables.getTable("FROGStick_values")
+        # TODO: see if the network tables is needed
+        # I think all objects are already place on NT
+        # self.nt = NetworkTables.getTable("{}_values".format(self.name))
 
-    def update_NT(self, control, value):
-        self.nt.putNumber(control, value)
+    # def update_NT(self, control, value):
+    #     self.nt.putNumber(control, value)
 
     @feedback
-    def get_speed(self):
-        # Dampens the -1 to 1 values of the joystick to provide a smoothed acceleration
+    def getSpeed(self):
+        # Dampens the -1 to 1 values of the joystick
+        # to provide a smoothed acceleration
         speed = self.getY()
         speed = -1 * (
-            speed ** 3 / self.SPEED_DIVISOR if abs(speed) > self.DEADBAND else 0
+            speed ** 3 / self.SPEED_DIVISOR
+            if abs(speed) > self.DEADBAND
+            else 0
         )
-        self.update_NT('speed', speed)
+    #    self.update_NT('speed', speed)
         return speed
-
-    @feedback
-    def get_throttle(self):
-        val = super().getThrottle()
-        self.update_NT('throttle', val)
-        return val
 
     @feedback
     def get_rotation(self):
@@ -58,30 +63,12 @@ class FROGStick(Joystick):
             else 0
         )
 
-    def getRangedCubedRotation(self):
-        return remap(
-            self.getTwist() ** 3,
-            self.SPEED_DIVISOR,
-            1,
-            self.ROTATION_MIN,
-            self.ROTATION_MAX,
-        )
-
-    def getRangeRotation(self):
-        return remap(
-            self.getTwist(),
-            self.SPEED_DIVISOR,
-            1,
-            self.ROTATION_MIN,
-            self.ROTATION_MAX,
-        )
-
-    def get_button(self, num):
+    def getButton(self, num):
         val = self.getRawButton(num)
-        self.update_NT('button_{}'.format(num), val)
+        # self.update_NT('button_{}'.format(num), val)
         return val
 
-    def get_debounced_button(self, num):
+    def getButtonDebounced(self, num):
         """Returns the value of the joystick button. If the button is held down, then
         True will only be returned once every ``debounce_period`` seconds"""
         val = False
@@ -90,8 +77,12 @@ class FROGStick(Joystick):
             if (now - self.button_latest.get(num, 0)) > self.DEBOUNCE_PERIOD:
                 self.button_latest[num] = now
                 val = True
-        self.update_NT('button_{}'.format(num), val)
+        # self.update_NT('button_{}'.format(num), val)
         return val
+
+
+class driveFROGStick(FROGStickBase):
+    pass
 
 
 class FROGXboxSimple(wpilib.XboxController):
@@ -111,10 +102,13 @@ class FROGXboxSimple(wpilib.XboxController):
 
     @feedback
     def get_speed(self):
-        # Dampens the -1 to 1 values of the joystick to provide a smoothed acceleration
+        # Dampens the -1 to 1 values of the joystick
+        # to provide a smoothed acceleration
         speed = self.getY(LEFTHAND)
         speed = -1 * (
-            speed ** 3 / self.SPEED_DIVISOR if abs(speed) > self.DEADBAND else 0
+            speed ** 3 / self.SPEED_DIVISOR
+            if abs(speed) > self.DEADBAND
+            else 0
         )
         return speed
 
@@ -160,11 +154,16 @@ class FROGXboxDriver(wpilib.XboxController):
 
     @feedback(key='Speed')
     def get_speed(self):
-        # Dampens the -1 to 1 values of the joystick to provide a smoothed acceleration
+        # Dampens the -1 to 1 values of the joystick
+        # to provide a smoothed acceleration
         forward = self.getTriggerAxis(RIGHTHAND)
         backward = self.getTriggerAxis(LEFTHAND)
         speed = forward - backward
-        speed = speed ** 3 / self.SPEED_DIVISOR if abs(speed) > self.DEADBAND else 0
+        speed = (
+            speed ** 3 / self.SPEED_DIVISOR
+            if abs(speed) > self.DEADBAND
+            else 0
+        )
         return speed
 
     @feedback(key='Rotation')
@@ -219,7 +218,9 @@ class FROGXboxGunner(wpilib.XboxController):
         super().__init__(channel)
         self.button_latest = {}
         self.timer = wpilib.Timer
-        self.nt = NetworkTables.getTable("components/driverstation/gunner_stick")
+        self.nt = NetworkTables.getTable(
+            "components/driverstation/gunner_stick"
+        )
 
     @feedback(key='Elevation')
     def get_elevation(self):
@@ -255,7 +256,8 @@ class FROGXboxGunner(wpilib.XboxController):
         val = -1
         now = self.timer.getFPGATimestamp()
         pov = self.getPOV()
-        # if button is pressed and we are outside the debounce period, set value
+        # if button is pressed and we are outside the debounce period,
+        # set value
         if pov > -1:
             if (now - self.button_latest.get('POV', 0)) > self.DEBOUNCE_PERIOD:
                 self.button_latest['POV'] = now
